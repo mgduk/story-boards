078cd4970419b27ae75b39e11a3e7c9f2bb77cdb
Matt Dolan <mdolan@trello.com>
2017-06-30 22:08:57 +0100

    Use port from env

--- server/src/index.js
+++ server/src/index.js
@@ -154,4 +154,5 @@ app.use((err, req, res, next) => {
   }
 });
 
-app.listen(3001, () => console.log('Server listening on 3001'));
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`Server listening on ${port}`));
