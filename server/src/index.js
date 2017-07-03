b118830a9cf692e360a7b2c088ddbfb5e7aeabef
Matt Dolan <mdolan@trello.com>
2017-07-03 18:09:07 +0100

    Whitelist API access and assert access to each object

--- server/src/index.js
+++ server/src/index.js
@@ -29,22 +29,18 @@ const getUserTrello = (userToken) => {
 // perform other write actions as storybot.
 const auth = (userToken, idBoard) => {
   const userTrello = getUserTrello(userToken);
-  if (authed[userToken] != null && authed[userToken].indexOf(idBoard) != -1) {
-    console.log('already authed');
+  if (authed[userToken] != null && authed[userToken].boards.indexOf(idBoard) != -1) {
     return Promise.resolve(userTrello);
   }
-  console.log('checking auth');
   return userTrello.putAsync(
     `/1/boards/${idBoard}/members/${config.idBotMember}`,
     { idMember: config.idBotMember, type: 'normal'}
   )
   .then(() => {
     if (authed[userToken] == null) {
-      authed[userToken] = [];
+      authed[userToken] = { boards: [], lists: [], cards: [], checklists: [] };
     }
-    if (authed[userToken].indexOf(idBoard) == -1) {
-      authed[userToken].push(idBoard);
-    }
+    authed[userToken].boards.push(idBoard);
     return userTrello;
   });
 };
@@ -103,6 +99,7 @@ app.post('/trello/boards', (req, res, next) => {
       })
       .catch(err => {
         console.log('board create err', err);
+        throw err;
       });
     };
     return createBoard(name);
@@ -117,19 +114,81 @@ app.all('/trello/*', (req, res, next) => {
   validateArgs(req.query)
   .then(() => auth(req.query.token, req.query.idBoard))
   .then((userTrello) => {
-    let trello, query;
-    const path = `/${req.params[0]}`;
+    let trello, query, prereq, ref;
+    const { token, idBoard } = req.query;
+    const path = `/${req.params[0]}`.toLowerCase();
     if (req.method == 'GET') {
       trello = userTrello;
       query = _.omit(req.query, 'idBoard', 'token');
-      console.log('GET as user')
-    } else {
+      prereq = Promise.resolve();
+    } else if (['PUT', 'POST', 'DELETE'].indexOf(req.method) !== -1) {
       trello = botTrello;
       query = req.body;
-      console.log(`${req.method} as bot`);
+
+      const disallow = () => { throw new Error('Not allowed'); }
+
+      const assertListIsOnBoard = (idList) => {
+        if (authed[token].lists.indexOf(idList) !== -1) {
+          return Promise.resolve();
+        }
+        return userTrello.getAsync(`/1/lists/${idList}`)
+        .then(list => (list.idBoard === idBoard) ? list : disallow())
+        .then(list => authed[token].lists.push(list.id))
+      }
+      const assertCardIsOnBoard = (idCard) => {
+        if (authed[token].cards.indexOf(idCard) !== -1) {
+          return Promise.resolve();
+        }
+        return userTrello.getAsync(`/1/cards/${idCard}`)
+        .then(card => assertListIsOnBoard(card.idList).then(() => card))
+        .then(card => authed[token].cards.push(card.id))
+      }
+      const assertChecklistIsOnBoard = (idChecklist) => {
+        if (authed[token].checklists.indexOf(idChecklist) !== -1) {
+          return Promise.resolve();
+        }
+        return userTrello.getAsync(`/1/checklists/${idChecklist}`)
+        .then(checklist => assertCardIsOnBoard(checklist.idCard).then(() => checklist))
+        .then(checklist => authed[token].checklists.push(checklist.id))
+      }
+
+      // /1/boards/…/
+      if (ref = path.match(new RegExp('^/1/boards?/([^/]+)(/|$)'))) {
+        if (ref[1] !== idBoard) {
+          disallow();
+        }
+        prereq = Promise.resolve();
+
+      } else if (path.match(/\/1\/lists?/)) {
+        /* passing idBoard so no further checks needed */
+        prereq = Promise.resolve();
+
+      // POST /1/cards
+      } else if (req.method == 'POST' && path == '/1/cards') {
+        prereq = assertListIsOnBoard(query.idList);
+
+      // /1/cards/…/
+      } else if (ref = path.match(new RegExp('^/1/cards?/([^/]+)(/|$)'))) {
+        prereq = assertCardIsOnBoard(ref[1]);
+
+      // /1/checklists/…
+      } else if (req.method == 'POST' && path.match(new RegExp('^/1/checklists?$'))) {
+        prereq = assertCardIsOnBoard(query.idCard);
+
+      // /1/checklists/…
+      } else if (ref = path.match(new RegExp('^/1/checklists?/([^/]+)(/|$)'))) {
+        prereq = assertChecklistIsOnBoard(ref[1]);
+
+      } else {
+        console.log('route fell through', path)
+        disallow();
+      }
+
+      console.log(`${req.method} ${path} as bot`);
+    } else {
+      throw new Error('Disallowed method');
     }
-    console.log(path, query);
-    return trello[`${req.method.toLowerCase()}Async`](path, query);
+    return prereq.then(() => trello[`${req.method.toLowerCase()}Async`](path, query));
   })
   .then(apiResponse => {
     res.json(apiResponse);
